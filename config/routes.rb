c31b45f7ec45fdc1effee5e47285e531d2528496
Ricardo Keigo <papeldeorigami@googlemail.com>
2014-01-16 13:39:27 -0200

    create admin controller for posts

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,13 @@ IslSite::Application.routes.draw do
   resources :posts
 
   get '/search', to: 'search#index'
+
+  resources 'posts', :only => [:index, :show]
   
+  namespace :admin do
+    resources :posts
+  end
+
   comfy_route :cms_admin
   # Make sure this routeset is defined last
   comfy_route :cms, :path => '/', :sitemap => false
--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -46,21 +46,6 @@ describe PostsController do
     end
   end
 
-  describe "GET new" do
-    it "assigns a new post as @post" do
-      get :new, {}, valid_session
-      assigns(:post).should be_a_new(Post)
-    end
-  end
-
-  describe "GET edit" do
-    it "assigns the requested post as @post" do
-      post = Post.create! valid_attributes
-      get :edit, {:id => post.to_param}, valid_session
-      assigns(:post).should eq(post)
-    end
-  end
-
   describe "POST create" do
     describe "with valid params" do
       it "creates a new Post" do
@@ -95,65 +80,6 @@ describe PostsController do
         post :create, {:post => { "headline" => "invalid value" }}, valid_session
         response.should render_template("new")
       end
-    end
-  end
-
-  describe "PUT update" do
-    describe "with valid params" do
-      it "updates the requested post" do
-        post = Post.create! valid_attributes
-        # Assuming there are no other posts in the database, this
-        # specifies that the Post created on the previous line
-        # receives the :update_attributes message with whatever params are
-        # submitted in the request.
-        Post.any_instance.should_receive(:update).with({ "headline" => "MyString" })
-        put :update, {:id => post.to_param, :post => { "headline" => "MyString" }}, valid_session
-      end
-
-      it "assigns the requested post as @post" do
-        post = Post.create! valid_attributes
-        put :update, {:id => post.to_param, :post => valid_attributes}, valid_session
-        assigns(:post).should eq(post)
-      end
-
-      it "redirects to the post" do
-        post = Post.create! valid_attributes
-        put :update, {:id => post.to_param, :post => valid_attributes}, valid_session
-        response.should redirect_to(post)
-      end
-    end
-
-    describe "with invalid params" do
-      it "assigns the post as @post" do
-        post = Post.create! valid_attributes
-        # Trigger the behavior that occurs when invalid params are submitted
-        Post.any_instance.stub(:save).and_return(false)
-        put :update, {:id => post.to_param, :post => { "headline" => "invalid value" }}, valid_session
-        assigns(:post).should eq(post)
-      end
-
-      it "re-renders the 'edit' template" do
-        post = Post.create! valid_attributes
-        # Trigger the behavior that occurs when invalid params are submitted
-        Post.any_instance.stub(:save).and_return(false)
-        put :update, {:id => post.to_param, :post => { "headline" => "invalid value" }}, valid_session
-        response.should render_template("edit")
-      end
-    end
-  end
-
-  describe "DELETE destroy" do
-    it "destroys the requested post" do
-      post = Post.create! valid_attributes
-      expect {
-        delete :destroy, {:id => post.to_param}, valid_session
-      }.to change(Post, :count).by(-1)
-    end
-
-    it "redirects to the posts list" do
-      post = Post.create! valid_attributes
-      delete :destroy, {:id => post.to_param}, valid_session
-      response.should redirect_to(posts_url)
     end
   end
 
